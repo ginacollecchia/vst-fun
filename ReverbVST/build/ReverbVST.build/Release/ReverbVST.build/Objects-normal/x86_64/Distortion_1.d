bd84a080d48d5f376b2f550bebb60529d4cf1e7a
ginacollecchia <ginacollecchia@gmail.com>
2020-06-02 13:57:47 -0600

    Cleaning up some duplicate files

--- ReverbVST/build/ReverbVST.build/Release/ReverbVST.build/Objects-normal/x86_64/Distortion_1.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/probraino/Coding/vst-fun/ReverbVST/source/Distortion_1.cpp \
-  /Users/probraino/Coding/vst-fun/ReverbVST/source/Distortion_1.h \
-  /Users/probraino/Coding/VST_SDK/VST2_SDK/public.sdk/source/vst2.x/audioeffectx.h \
-  /Users/probraino/Coding/VST_SDK/VST2_SDK/public.sdk/source/vst2.x/audioeffect.h \
-  /Users/probraino/Coding/VST_SDK/VST2_SDK/pluginterfaces/vst2.x/aeffect.h \
-  /Users/probraino/Coding/VST_SDK/VST2_SDK/pluginterfaces/vst2.x/aeffectx.h
